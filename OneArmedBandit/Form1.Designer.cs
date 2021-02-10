95610afe1c07ea98bd37cf44ed9d9623699e9d9f
Mr. T <tony.theodoropoulos@ed.amdsb.ca>
2021-02-10 15:32:07 -0500

    changed design to use PictureBoxes with zoom

--- OneArmedBandit/Form1.Designer.cs
+++ OneArmedBandit/Form1.Designer.cs
@@ -32,10 +32,13 @@
             this.spinButton = new System.Windows.Forms.Button();
             this.outputLabel = new System.Windows.Forms.Label();
             this.scoreDisplay = new System.Windows.Forms.Label();
-            this.reel3 = new System.Windows.Forms.Label();
-            this.reel2 = new System.Windows.Forms.Label();
-            this.reel1 = new System.Windows.Forms.Label();
             this.coinLabel = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // spinButton
@@ -43,9 +46,10 @@
             this.spinButton.BackColor = System.Drawing.Color.LimeGreen;
             this.spinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.spinButton.Font = new System.Drawing.Font("Rockwell", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.spinButton.Location = new System.Drawing.Point(478, 188);
+            this.spinButton.Location = new System.Drawing.Point(319, 122);
+            this.spinButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.spinButton.Name = "spinButton";
-            this.spinButton.Size = new System.Drawing.Size(77, 71);
+            this.spinButton.Size = new System.Drawing.Size(51, 46);
             this.spinButton.TabIndex = 3;
             this.spinButton.Text = "SPIN";
             this.spinButton.UseVisualStyleBackColor = false;
@@ -56,9 +60,10 @@
             this.outputLabel.BackColor = System.Drawing.Color.DimGray;
             this.outputLabel.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.outputLabel.ForeColor = System.Drawing.Color.White;
-            this.outputLabel.Location = new System.Drawing.Point(156, 363);
+            this.outputLabel.Location = new System.Drawing.Point(104, 236);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(243, 48);
+            this.outputLabel.Size = new System.Drawing.Size(162, 31);
             this.outputLabel.TabIndex = 4;
             this.outputLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
@@ -67,82 +72,93 @@
             this.scoreDisplay.BackColor = System.Drawing.Color.Transparent;
             this.scoreDisplay.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.scoreDisplay.ForeColor = System.Drawing.Color.White;
-            this.scoreDisplay.Location = new System.Drawing.Point(89, 363);
+            this.scoreDisplay.Location = new System.Drawing.Point(59, 236);
+            this.scoreDisplay.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.scoreDisplay.Name = "scoreDisplay";
-            this.scoreDisplay.Size = new System.Drawing.Size(61, 48);
+            this.scoreDisplay.Size = new System.Drawing.Size(41, 31);
             this.scoreDisplay.TabIndex = 5;
             this.scoreDisplay.Text = "10";
             this.scoreDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // reel3
-            // 
-            this.reel3.BackColor = System.Drawing.Color.White;
-            this.reel3.Image = global::OneArmedBandit.Properties.Resources.diamond_100x125;
-            this.reel3.Location = new System.Drawing.Point(345, 174);
-            this.reel3.Name = "reel3";
-            this.reel3.Size = new System.Drawing.Size(100, 94);
-            this.reel3.TabIndex = 2;
-            // 
-            // reel2
-            // 
-            this.reel2.BackColor = System.Drawing.Color.White;
-            this.reel2.Image = global::OneArmedBandit.Properties.Resources.cherry_100x125;
-            this.reel2.Location = new System.Drawing.Point(229, 174);
-            this.reel2.Name = "reel2";
-            this.reel2.Size = new System.Drawing.Size(100, 94);
-            this.reel2.TabIndex = 1;
-            // 
-            // reel1
-            // 
-            this.reel1.BackColor = System.Drawing.Color.White;
-            this.reel1.Image = global::OneArmedBandit.Properties.Resources._7_100x125;
-            this.reel1.Location = new System.Drawing.Point(114, 174);
-            this.reel1.Name = "reel1";
-            this.reel1.Size = new System.Drawing.Size(99, 94);
-            this.reel1.TabIndex = 0;
             // 
             // coinLabel
             // 
             this.coinLabel.BackColor = System.Drawing.Color.Transparent;
             this.coinLabel.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.coinLabel.ForeColor = System.Drawing.Color.White;
-            this.coinLabel.Location = new System.Drawing.Point(16, 362);
+            this.coinLabel.Location = new System.Drawing.Point(11, 235);
+            this.coinLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.coinLabel.Name = "coinLabel";
-            this.coinLabel.Size = new System.Drawing.Size(84, 48);
+            this.coinLabel.Size = new System.Drawing.Size(56, 31);
             this.coinLabel.TabIndex = 6;
             this.coinLabel.Text = "Coins:";
             this.coinLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.BackgroundImage = global::OneArmedBandit.Properties.Resources.diamond_100x125;
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(235, 116);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(54, 56);
+            this.pictureBox3.TabIndex = 12;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::OneArmedBandit.Properties.Resources.cherry_100x125;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(159, 116);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(54, 56);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::OneArmedBandit.Properties.Resources._7_100x125;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(84, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 56);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::OneArmedBandit.Properties.Resources.slotBack;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(559, 413);
+            this.ClientSize = new System.Drawing.Size(373, 268);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.coinLabel);
             this.Controls.Add(this.scoreDisplay);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.spinButton);
-            this.Controls.Add(this.reel3);
-            this.Controls.Add(this.reel2);
-            this.Controls.Add(this.reel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Central Slots";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Label reel1;
-        private System.Windows.Forms.Label reel2;
-        private System.Windows.Forms.Label reel3;
         private System.Windows.Forms.Button spinButton;
         private System.Windows.Forms.Label outputLabel;
         private System.Windows.Forms.Label scoreDisplay;
         private System.Windows.Forms.Label coinLabel;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
